eab84ed491fe33a3118101d17f9b316283c27b51
FilipDavid1 <filipenkodavid@gmail.com>
2022-09-22 15:40:35 +0200

    add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        dark: '#8FA5B2',
+        mid: '#C4D0C6',
+        light: '#E3E9E9',
+        superLight: '#EAE9E7',
+        white: '#FFFFFF',
+        black: '#000000',
+      }
+    },
   },
   plugins: [],
 }
