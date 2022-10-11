f867c5f40aa1a6b525228564abc365e1bf6f3270
FilipDavid1 <filipenkodavid@gmail.com>
2022-10-11 09:15:25 +0200

    add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['../src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
+
+  prefix: '',
+
+      content: [
+        './src/**/*.{html,ts}',
+      ],
+
   
   theme: {
     extend: {
